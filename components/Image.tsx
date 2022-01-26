ebdf32596099c28450e70f758ec3f741e44ba00b
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-01-25 20:09:44 -0500

    use custom image loader

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,5 @@
 import NextImage, { ImageProps } from 'next/image';
-
+// https://github.com/vercel/next.js/discussions/19065
 const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} loader={customLoader} />;
 
 // opt-out of image optimization, no-op
