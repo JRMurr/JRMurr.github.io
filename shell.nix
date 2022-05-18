46f968b02f39c9e9cfe46aa9f8361b5bdf796200
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-05-17 21:34:00 -0400

    switch to flake

--- shell.nix
+++ shell.nix
@@ -1,3 +1,7 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.mkShell { buildInputs = with pkgs; [ nodejs-16_x just ]; }
+# still support `nix-shell` if user does not have nix flakes. flake-compat makes the flake backwards compatible
+(import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+in fetchTarball {
+  url =
+    "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+  sha256 = lock.nodes.flake-compat.locked.narHash;
+}) { src = ./.; }).shellNix
