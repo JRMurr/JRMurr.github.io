7ed20393c8d6d77bf0030890866829d082230ab4
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-08-02 08:21:42 -0400

    add twitter

--- data/authors/default.md
+++ data/authors/default.md
@@ -3,6 +3,7 @@ name: John Murray
 avatar: /static/images/me.jpg
 occupation: Software Engineer
 github: https://github.com/JRMurr
+twitter: https://twitter.com/JRMurrCodes
 ---
 
 Will ramble on types, rust, and nix as much as humanely possible.
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -12,7 +12,7 @@ const siteMetadata = {
   socialBanner: '/static/images/twitter-card.png',
   // email: 'address@yoursite.com',
   github: 'https://github.com/JRMurr',
-  // twitter: 'https://twitter.com/Twitter',
+  twitter: 'https://twitter.com/JRMurrCodes',
   // facebook: 'https://facebook.com',
   // youtube: 'https://youtube.com',
   // linkedin: 'https://www.linkedin.com',
