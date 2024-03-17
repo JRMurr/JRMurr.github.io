0437a08a09af02b85be2fef515625fbfdbe52df9
John Murray <5672686+JRMurr@users.noreply.github.com>
2024-03-17 19:03:35 -0400

    setup giscus

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-// import { Comments as CommentsComponent } from 'pliny/comments/index.js'
 import { useState } from 'react'
 import siteMetadata from '@/content/siteMetadata'
+import { Giscus } from './comments/giscus'
 
-export default function Comments({ slug }: { slug: string }) {
+export default function Comments() {
   const [loadComments, setLoadComments] = useState(false)
   return (
     <>
       {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
-      {/* @MIGRATE TODO: this */}
-      {/* {siteMetadata.comments && loadComments && (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      )} */}
+      {siteMetadata.comments && loadComments && <Giscus {...siteMetadata.comments.giscusConfig} />}
     </>
   )
 }
--- content/siteMetadata.ts
+++ content/siteMetadata.ts
@@ -1,4 +1,3 @@
-/** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: "John's Codes",
   author: 'John Murray',
@@ -53,18 +52,17 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      // @MIGRATE TODO: this
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'JRMurr/JRMurr.github.io',
+      repositoryId: 'R_kgDOGuvrNw',
+      category: 'Comments',
+      categoryId: 'DIC_kwDOGuvrN84CPZ0a',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: 'preferred_color_scheme',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
@@ -89,6 +87,6 @@ const siteMetadata = {
     //   indexName: 'docsearch',
     // },
   },
-}
+} as const
 
 export default siteMetadata
